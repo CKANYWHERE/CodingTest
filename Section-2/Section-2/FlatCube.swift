4bf1386b1c6332fd4acae3cb94e403e6cac50fac
Min-Chang-Kyeoung <janu723@naver.com>
2020-12-07 15:20:13 +0900

    Complete Test2

---
+++ Section-2/Section-2/FlatCube.swift
@@ -0,0 +1,148 @@
+//
+//  Logic.swift
+//  Section-2
+//
+//  Created by 민창경 on 2020/12/07.
+//
+
+import Foundation
+
+class FlatCube{
+    
+    func loadData() ->[[String]]{
+        let cube =
+        [
+            ["R","R","W"],
+            ["G","C","W"],
+            ["G","B","B"]
+        ]
+        
+        return cube
+    }
+    
+    func U(cube:[[String]]) -> [[String]]{
+        let first = cube[0][0]
+        let second = cube[0][1]
+        let third = cube[0][2]
+        return [
+            [second,third,first],
+            cube[1],
+            cube[2]
+            ]
+        
+    }
+
+    func Uquote(cube:[[String]]) -> [[String]]{
+        let first = cube[0][0]
+        let second = cube[0][1]
+        let third = cube[0][2]
+        return[
+            [third,first,second]
+            ,cube[1]
+            ,cube[2]
+        ]
+    }
+
+    func R(cube:[[String]]) -> [[String]]{
+        let first = cube[0][2]
+        let second = cube[1][2]
+        let third = cube[2][2]
+        
+        return[
+           [cube[0][0],cube[0][1],second]
+            ,[cube[1][0],cube[1][1],third]
+            ,[cube[2][0],cube[2][1],first]
+        ]
+        
+    }
+
+    func Rquote(cube:[[String]]) -> [[String]]{
+        let first = cube[0][2]
+        let second = cube[1][2]
+        let third = cube[2][2]
+        
+        return[
+           [cube[0][0],cube[0][1],third]
+            ,[cube[1][0],cube[1][1],first]
+            ,[cube[2][0],cube[2][1],second]
+        ]
+    }
+
+    func L(cube:[[String]]) -> [[String]]{
+        let first = cube[0][0]
+        let second = cube[1][0]
+        let third = cube[2][0]
+        
+        return[
+           [third,cube[0][1],cube[0][2]]
+            ,[first,cube[1][1],cube[1][2]]
+            ,[second,cube[2][1],cube[2][2]]
+        ]
+    }
+
+    func Lquote(cube:[[String]]) -> [[String]]{
+        let first = cube[0][0]
+        let second = cube[1][0]
+        let third = cube[2][0]
+        
+        return[
+            [second,cube[0][1],cube[0][2]]
+             ,[third,cube[1][1],cube[1][2]]
+             ,[first,cube[2][1],cube[2][2]]
+         ]
+    }
+
+    func B(cube:[[String]])-> [[String]]{
+        let first = cube[2][0]
+        let second = cube[2][1]
+        let third = cube[2][2]
+        return[
+            cube[0]
+            ,cube[1]
+            ,[third,first,second]
+        ]
+    }
+
+    func Bquote(cube:[[String]]) -> [[String]]{
+        let first = cube[2][0]
+        let second = cube[2][1]
+        let third = cube[2][2]
+        return [
+            cube[0],
+            cube[1],
+            [second,third,first]
+            ]
+    }
+    
+    
+    //MARK: 커맨드 라인 입력한 부분 파싱하기
+    func parseCmd(cmd : String) -> [String]{
+        var cmds:[String] = []
+        for i in 0...cmd.count - 1{
+            var getCmds = ""
+            //SingleQutation일 경우 처리 안함
+            if String(cmd[cmd.index(cmd.startIndex,offsetBy:i)]) != "'"{
+                
+                if i != cmd.count - 1 {
+                    //실행해야할 커맨드에 싱글쿼테이션이 있는경우
+                    if String(cmd[cmd.index(cmd.startIndex,offsetBy:i+1)]) == "'"{
+                        getCmds = String(cmd[cmd.index(cmd.startIndex,offsetBy:i)]) + "'"
+                    }
+                    //없는경우
+                    else{
+                        getCmds = (String(cmd[cmd.index(cmd.startIndex,offsetBy:i)]))
+                    }
+                }
+                //마지막 커맨드
+                else{
+                    getCmds = (String(cmd[cmd.index(cmd.startIndex,offsetBy:i)]))
+                }
+                
+                cmds.append(getCmds)
+            }
+        }
+        return cmds
+    }
+}
+
+
--- Section-2/Section-2/main.swift
+++ Section-2/Section-2/main.swift
@@ -7,14 +7,59 @@
 
 import Foundation
 
-var cube =
-[
-    ["R","R","W"],
-    ["G","C","W"],
-    ["G","B","B"]
-]
+let cube = FlatCube()
+var data = cube.loadData()
 
-print(cube)
+print(data[0])
+print(data[1])
+print(data[2])
 
+var cmd = ""
+while(cmd != "Q"){
+    print("Cube>")
+    cmd = readLine()!
+    
+    let cmds = cube.parseCmd(cmd: cmd)
+    
+    cmds.forEach{
+        if $0 == "U'"{
+            print("U'")
+            data = cube.Uquote(cube: data)
+        }
+        else if $0 == "U"{
+            print("U")
+            data = cube.U(cube: data)
+        }
+        else if $0 == "R"{
+            print("R")
+            data = cube.R(cube: data)
+        }
+        else if $0 == "R'"{
+            print("R'")
+            data = cube.Rquote(cube: data)
+        }
+        else if $0 == "L"{
+            print("L")
+            data = cube.L(cube: data)
+        }
+        else if $0 == "L'"{
+            print("L'")
+            data = cube.Lquote(cube: data)
+        }
+        else if $0 == "B"{
+            print("B")
+            data = cube.B(cube: data)
+        }
+        else if $0 == "B'"{
+            print("B'")
+            data = cube.Bquote(cube: data)
+        }
+        
+        print(data[0])
+        print(data[1])
+        print(data[2])
 
+    }
+}
 
+print("Bye~")
